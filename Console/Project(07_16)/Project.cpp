822fa717ea9545e650761923ebfea54e315c2da0
dudgus614@naver.com <dudgus614@naver.com>
2024-07-16 20:10:59 +0900

    홍영현 : 커밋

--- Console/Project(07_16)/Project.cpp
+++ Console/Project(07_16)/Project.cpp
@@ -15,8 +15,8 @@ void Character()
 	std::cout << "□□□□□□□□□□□□■■■■■■■■■■■■■■■■■■■■■■□□□□□□□□□□□□□□□□" << std::endl;
 	std::cout << "□□□□□□□□□□■■■■■■■■■■■■■■■■■■■■■■■■■□□□□□□□□□□□□□□□" << std::endl;
 	std::cout << "□□□□□□□□□□■■□□□□□□■■■■■■□□□□□□■■■■■■□□□□□□□□□□□□□□" << std::endl;
-	std::cout << "□□□□□□□□□□■■□□●●□□■■■■□□□□●●□□□□■■■■□□□□□□□□□□□□□□" << std::endl;
-	std::cout << "□□□□□□□□□□■■□□●●□□□□□□□□□□●●□□□□■■■■□□□□□□□□□□□□□□" << std::endl;
+	std::cout << "□□□□□□□□□□■■□□■■□□■■■■□□□□■■□□□□■■■■□□□□□□□□□□□□□□" << std::endl;
+	std::cout << "□□□□□□□□□□■■□□■■□□□□□□□□□□■■□□□□■■■■□□□□□□□□□□□□□□" << std::endl;
 	std::cout << "□□□□□□□□□□■■□□□□□□▼▼▼▼□□□□□□□□□□■■■■□□□□□□□□□□□□□□" << std::endl;
 	std::cout << "□□□□□□□□□□□□■■□□□□▲▲▲▲□□□□□□□□□□■■□□□□□□□□□□□□□□□□" << std::endl;
 	std::cout << "□□□□□□□□□□□□□□■■□□□□□□□□□□□□□□■■■■■■□□□□□□□□□□□□□□" << std::endl;
@@ -47,10 +47,15 @@ void Character()
 	std::cout << "□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□" << std::endl;
 }
 
-void Gotoxy(int X, int Y, int Xlength) 
+void Gotoxy(short X, short Y)
 {
 	COORD pos = { X, Y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void NemoPrint(int X, int Y, int Xlength) 
+{
+	Gotoxy(X, Y);
 
 	for (int i = 0; i < Xlength; i++)
 	{
@@ -59,60 +64,138 @@ void Gotoxy(int X, int Y, int Xlength)
 	std::cout << std::endl;
 }
 
+void RevTriPrint(int X, int Y, int Xlength)
+{
+	Gotoxy(X, Y);
+
+	for (int i = 0; i < Xlength; i++)
+	{
+		std::cout << "▼▼";
+	}
+	std::cout << std::endl;
+}
+
+void TriPrint(int X, int Y, int Xlength)
+{
+	Gotoxy(X, Y);
+
+	for (int i = 0; i < Xlength; i++)
+	{
+		std::cout << "▲▲";
+	}
+	std::cout << std::endl;
+}
+
 void CharacterXY()
 {
-	Gotoxy(17, 4, 5);
-	Gotoxy(13, 5, 9);
-	Gotoxy(11, 6, 11);
-	Gotoxy(11, 7, 11);
-	Gotoxy(9, 8, 13);
-	Gotoxy(9, 9, 1); Gotoxy(17, 9, 3); Gotoxy(29, 9, 3);
-	Gotoxy(9, 10, 1); Gotoxy(13, 10, 1); Gotoxy(17, 10, 2); Gotoxy(25, 10, 1); Gotoxy(31, 10, 2);
-	Gotoxy(9, 11, 1); Gotoxy(13, 11, 1); Gotoxy(17, 11, 2); Gotoxy(25, 11, 1); Gotoxy(31, 11, 2);
-	Gotoxy(9, 12, 1);  Gotoxy(31, 12, 2);
-	Gotoxy(11, 13, 1);  Gotoxy(31, 13, 1);
-	Gotoxy(13, 14, 1); Gotoxy(29, 14, 3);
-	Gotoxy(11, 15, 1); Gotoxy(15, 15, 10);
-	Gotoxy(9, 16, 2); Gotoxy(35, 16, 1);
-	Gotoxy(7, 17, 1); Gotoxy(11, 17, 1); Gotoxy(35, 17, 1);
-	Gotoxy(7, 18, 1); Gotoxy(11, 18, 1); Gotoxy(27, 18, 1); Gotoxy(37, 18, 1);
-	Gotoxy(5, 19, 1); Gotoxy(9, 19, 1); Gotoxy(27, 19, 1); Gotoxy(39, 19, 1);
-	Gotoxy(5, 20, 1); Gotoxy(9, 20, 1); Gotoxy(27, 20, 1); Gotoxy(33, 20, 1); Gotoxy(39, 20, 1);
-	Gotoxy(3, 21, 1); Gotoxy(9, 21, 1); Gotoxy(27, 21, 1); Gotoxy(33, 21, 1); Gotoxy(39, 21, 1);
-	Gotoxy(3, 22, 1); Gotoxy(7, 22, 1); Gotoxy(29, 22, 1); Gotoxy(33, 22, 1); Gotoxy(41, 22, 1);
-	Gotoxy(5, 23, 2); Gotoxy(31, 23, 2); Gotoxy(41, 23, 1);
-	Gotoxy(7, 24, 1); Gotoxy(41, 24, 1);
-	Gotoxy(7, 25, 1); Gotoxy(41, 25, 1);
-	Gotoxy(7, 26, 1); Gotoxy(31, 26, 1); Gotoxy(39, 26, 1);
-	Gotoxy(7, 27, 1); Gotoxy(11, 27, 1); Gotoxy(29, 27, 1); Gotoxy(37, 27, 1);
-	Gotoxy(9, 28, 3); Gotoxy(27, 28, 3); Gotoxy(35, 28, 1);
-	Gotoxy(9, 29, 1); Gotoxy(15, 29, 3);
+	NemoPrint(15, 2, 5);
+	NemoPrint(11, 3, 9);
+	NemoPrint(9, 4, 11);
+	NemoPrint(9, 5, 11);
+	NemoPrint(7, 6, 13);
+	NemoPrint(7, 7, 1); NemoPrint(15, 7, 3); NemoPrint(27, 7, 3);
+	NemoPrint(7, 8, 1); NemoPrint(11, 8, 1); NemoPrint(15, 8, 2); NemoPrint(23, 8, 1); NemoPrint(29, 8, 2);
+	NemoPrint(7, 9, 1); NemoPrint(11, 9, 1); NemoPrint(15, 9, 2); NemoPrint(23, 9, 1); NemoPrint(29, 9, 2);
+	NemoPrint(7, 10, 1);  NemoPrint(29, 10, 2);
+	NemoPrint(9, 11, 1);  NemoPrint(29, 11, 1);
+	NemoPrint(11, 12, 1); NemoPrint(27, 12, 3);
+	NemoPrint(9, 13, 1); NemoPrint(13, 13, 10);
+	NemoPrint(7, 14, 2); NemoPrint(33, 14, 1);
+	NemoPrint(5, 15, 1); NemoPrint(9, 15, 1); NemoPrint(33, 15, 1);
+	NemoPrint(5, 16, 1); NemoPrint(9, 16, 1); NemoPrint(25, 16, 1); NemoPrint(35, 16, 1);
+	NemoPrint(3, 17, 1); NemoPrint(7, 17, 1); NemoPrint(25, 17, 1); NemoPrint(37, 17, 1);
+	NemoPrint(3, 18, 1); NemoPrint(7, 18, 1); NemoPrint(25, 18, 1); NemoPrint(31, 18, 1); NemoPrint(37, 18, 1);
+	NemoPrint(1, 19, 1); NemoPrint(7, 19, 1); NemoPrint(25, 19, 1); NemoPrint(31, 19, 1); NemoPrint(37, 19, 1);
+	NemoPrint(1, 20, 1); NemoPrint(5, 20, 1); NemoPrint(27, 20, 1); NemoPrint(31, 20, 1); NemoPrint(39, 20, 1);
+	NemoPrint(3, 21, 2); NemoPrint(29, 21, 2); NemoPrint(39, 21, 1);
+	NemoPrint(5, 22, 1); NemoPrint(39, 22, 1);
+	NemoPrint(5, 23, 1); NemoPrint(39, 23, 1);
+	NemoPrint(5, 24, 1); NemoPrint(29, 24, 1); NemoPrint(37, 24, 1);
+	NemoPrint(5, 25, 1); NemoPrint(9, 25, 1); NemoPrint(27, 25, 1); NemoPrint(35, 25, 1);
+	NemoPrint(7, 26, 3); NemoPrint(25, 26, 3); NemoPrint(33, 26, 1);
+	NemoPrint(7, 27, 1); NemoPrint(13, 27, 6); NemoPrint(31, 27, 1);
+	NemoPrint(9,28, 2); NemoPrint(25, 28, 3);
+
+	// 입
+	RevTriPrint(15, 10, 2);
+	TriPrint(15, 11, 2);
 }
 
 // 2. 과제
 void Myself()
 {
-	std::cout << "이름 : 홍영현 " << std::endl;
-	std::cout << "나이 : 28" << std::endl;
+	std::cout << "========================================================" << std::endl;
+	std::cout << "이름   : 홍영현 " << std::endl;
+	std::cout << "나이   : 28" << std::endl;
 	std::cout << "연락처 : 010-3424-1433" << std::endl;
-	std::cout << "주소 : 서울특별시 상원 1길 17 한양현대 APT 101동 403호" << std::endl;
+	std::cout << "주소   : 서울특별시 상원 1길 17 한양현대 APT 101동 403호" << std::endl;
 	std::cout << "이메일 : dudgus614@naver.com" << std::endl;
-	std::cout << "각오 : 목표하는 회사에 들어가겠습니다." << std::endl;
+	std::cout << "각오   : 목표하는 회사에 들어가겠습니다." << std::endl;
 	std::cout << "가고싶은 회사  : 넥슨" << std::endl;
 	std::cout << "만들고싶은게임 : 로스트아크" << std::endl;
-	std::cout << "취미 : 게임하는 것" << std::endl;
+	std::cout << "취미   : 게임하는 것" << std::endl;
+	std::cout << "========================================================" << std::endl;
 }
 
 // 3. 도전과제
+#include <string.h>
+class Introduce
+{
 
-void main()
+public :
+	void PrintIntroduceMyself()
+	{
+		std::cout << "========================================================" << std::endl;
+		std::cout << "이름   : " << Name << std::endl;
+		std::cout << "나이   : " << Age << std::endl;
+		std::cout << "연락처 : " << PhoneNum << std::endl;
+		std::cout << "주소   : " << Address << std::endl;
+		std::cout << "이메일 : " << Email << std::endl;
+		std::cout << "각오   : " << Resolution << std::endl;
+		std::cout << "가고싶은 회사  : " << WCompany << std::endl;
+		std::cout << "만들고싶은게임 : " << WMGame << std::endl;
+		std::cout << "취미   : " << Hobby << std::endl;
+		std::cout << "========================================================";
+	}
+
+public:
+	std::string Name = " ";
+	std::string Age = " ";
+	std::string PhoneNum = " ";
+	std::string Address = " ";
+	std::string Email = " ";
+	std::string Resolution = " ";
+	std::string WCompany = " ";
+	std::string WMGame = " ";
+	std::string Hobby = " ";
+};
+
+
+int main()
 {
 	// 1. 과제. 노가다
 	//Character();
 
 	// 1. 과제. 좌표
-	CharacterXY();
+	//CharacterXY();
 
 	// 2. 과제
-	//Myself();
+	Myself();
+
+	// 1. 도전과제
+	Introduce Myself;
+
+	Myself.Name = "홍영현";
+	Myself.Age = "28";
+	Myself.PhoneNum = "010-3424-1433";
+	Myself.Email = "dudgus614@naver.com";
+	Myself.Address = "서울특별시 상원 1길 17 한양현대 APT 101동 403호";
+	Myself.Resolution = "목표하는 회사에 들어가겠습니다.";
+	Myself.WCompany = "넥슨";
+	Myself.WMGame = "로스트아크";
+	Myself.Hobby = "게임하는 것";
+
+	Myself.PrintIntroduceMyself();
+
+	return 0;
 }
