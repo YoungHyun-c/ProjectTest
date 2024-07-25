9c68c14b4e0cf850d4e38d0bfd84beada8c0a39c
dudgus614@naver.com <dudgus614@naver.com>
2024-07-25 23:53:24 +0900

    홍영현 : 카드게임 커밋

--- Console/Project(07_25)/Project8.cpp
+++ Console/Project(07_25)/Project8.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <random>
 #include <algorithm>
-#include <vector>
+#include <windows.h>
 
 enum class CardNum
 {
@@ -10,7 +10,6 @@ enum class CardNum
 	Heart = 2,
 	Diamond = 3,
 };
-// J = 11, Q = 12, K = 13
 
 struct Cards
 {
@@ -55,73 +54,111 @@ void Cards::CardDeclareNum()
 	}
 }
 
+void CardPrint(std::string _WhoCard, int _CurIndex, int _Num, Cards _Card);
+
 
 void CardGame()
 {
 	Cards Card;
 	Card.CardDeclareNum();
-	std::cout << "카드 : ";
-	std::cout << std::endl;
-	for (int j = 0; j < 4; j++)
-	{
-		for (int k = 0; k < 13; k++)
-		{
-			std::cout << Card.CardArr[j][k] << " ";
-		}
-		std::cout << std::endl;
-	}
 
-	std::random_device Rd;
-	std::mt19937 gen(Rd());
-	std::uniform_int_distribution<> dist(0, 3);
 	int CardCheck[53] = {};
-	for (int i = 1; i < 53; i++)
+	for (int i = 0; i < 52; i++)
 	{
 		CardCheck[i] = i;
 	}
 	RandomNum(CardCheck, 0, 51);
+	int CardLeft = Card.CardValue;
 
 	int PlayerMoney = 10000;
 	int Index = 0;
-	for (int i = 0; i < 2; i++)
+	int PlayerBatting = 100;
+	int WinBatting = 3;
+	int LoseBatting = 2;
+	std::string WhoCard = "";
+
+	std::cout << "카드 게임에 오신걸 환영합니다~~~" << std::endl;
+	for (int i = 0; i < 17 && 1 < CardLeft && 0 < PlayerMoney; i++)
 	{
-		int ChoiceCard = (CardCheck[Index] / 13);
-		if (ChoiceCard == 4)
+		std::cout << "남은 소지금 : " << PlayerMoney << std::endl;
+		printf("배팅 금액을 입력해주세요 (100 ~ %d) : ", PlayerMoney);
+		std::cin >> PlayerBatting;
+		while (PlayerBatting < 100 || PlayerBatting > PlayerMoney)
 		{
-			ChoiceCard = 3;
+			printf("최소 배팅금액과 당신의 소지금 내에서 다시 입력하세요! (100 ~ %d) : ", PlayerMoney);
+			std::cin >> PlayerBatting;
 		}
-		int CardNumber = ((CardCheck[Index] % 13));
-		int PlayerBatting = 1000;
-		//std::cout << "배팅 금액을 입력해주세요 : ";
-		//std::cin >> PlayerBatting;
+		int ComCard[2] = {};
+		int PlayerCard = 0;
 
-		switch (ChoiceCard)
+		for (int CurIndex = 0; CurIndex < 3; CurIndex++)
 		{
-		case static_cast<int>(CardNum::Clover):
+			int ChoiceCard = (CardCheck[Index] / 13);
+			int CardNumber = ((CardCheck[Index] % 13));
+			switch (ChoiceCard)
+			{
+			case static_cast<int>(CardNum::Clover):
+				Card.CardObject = "♧";
+				break;
+			case static_cast<int>(CardNum::Spade):
+				Card.CardObject = "♤";
+				break;
+			case static_cast<int>(CardNum::Heart):
+				Card.CardObject = "♡";
+				break;
+			case static_cast<int>(CardNum::Diamond):
+				Card.CardObject = "◆";
+				break;
+			default:
+				break;
+			}
 
-			Card.CardObject = "♧";
-			break;
-		case static_cast<int>(CardNum::Spade):
-			Card.CardObject = "♤";
-			break;
-		case static_cast<int>(CardNum::Heart):
-			Card.CardObject = "♡";
-			break;
-		case static_cast<int>(CardNum::Diamond):
-			Card.CardObject = "◇";
-			break;
-		default:
+			int CurCard = Card.CardArr[ChoiceCard][CardNumber];
+			CardLeft--;
+
+			if (CurIndex < 2)
+			{
+				WhoCard = "컴퓨터 카드 : ";
+				ComCard[CurIndex] = CurCard;
+			}
+
+			if (CurIndex == 2)
+			{
+				WhoCard = "플레이어 카드 : ";
+				PlayerCard = CurCard;
+			}
+
+			CardPrint(WhoCard, CurIndex, CurCard, Card);
+			Index++;
+		}
+
+		std::sort(ComCard, ComCard + 2);
+		printf("컴퓨터 카드 : %d ~ %d, 플레이어 카드 : %d\n", ComCard[0], ComCard[1], PlayerCard);
+		if (ComCard[0] < PlayerCard && PlayerCard < ComCard[1])
+		{
+			std::cout << "승리하셨습니다!!!" << std::endl;
+			printf("소지금 : %d + %d X %d\n", PlayerMoney, PlayerBatting, WinBatting);
+			PlayerMoney += PlayerBatting * WinBatting;
+			std::cout << "당신의 소지금 : " << PlayerMoney << std::endl;
+		}
+		else
+		{
+			std::cout << "졌습니다..." << std::endl;
+			printf("소지금 : %d - %d X %d\n", PlayerMoney, PlayerBatting, LoseBatting);
+			PlayerMoney -= (PlayerBatting * LoseBatting);
+			std::cout << "당신의 소지금 : " << PlayerMoney << std::endl;
+		}
+
+		if (PlayerMoney <= 0 || CardLeft <= 1)
+		{
+			std::cout << "게임이 끝났습니다..." << std::endl;
 			break;
 		}
-		int CurCard = Card.CardArr[ChoiceCard][CardNumber];
 
-		std::cout << "카드 뽑은 횟수 : "<< i + 1 << std::endl;
-		std::cout << "초이스 : " << ChoiceCard << std::endl;
-		std::cout << "넘버 : " << CardNumber << std::endl;
-		std::cout << "컴퓨터 카드 : ";
-		std::cout << Card.CardObject << " ." << CurCard << std::endl;
+		Sleep(5000);
+		std::system("Cls");
 
-		Index++;
+		std::cout << "남은 카드 : " << CardLeft << std::endl;
 	}
 }
 
@@ -133,40 +170,32 @@ int main()
 	return 0;
 }
 
-
-//std::cout << "플레이어 카드 : ";
-//std::cout << Card.CardObject << " ." << Card.CardArr[ChoiceCard][CardNumber] << std::endl;
-//PlayerNum = Card.CardArr[ChoiceCard][CardNumber];
-//CardNumber++;
-//Index += 2;
-//
-//if (PlayerNum > ComArr[0] || PlayerNum < ComArr[1])
-//{
-//	std::cout << "승리하셨습니다." << std::endl;
-//	printf("소지금 %d + 배팅금액 %d X 3 당신의 소지금 : %d", PlayerMoney, PlayerBatting, PlayerMoney);
-//}
-
-
-	/*for (int i = 0; i < 5; i++)
+void CardPrint(std::string _WhoCard, int _CurIndex, int _CurCard, Cards _Card)
+{
+	if (_CurCard <= 10)
 	{
-		for (int j = 0; j < 14; j++)
+		std::cout << _WhoCard;
+		std::cout << _Card.CardObject << " ." << _CurCard << std::endl;
+	}
+	else if (_CurCard > 10)
+	{
+		// J = 11, Q = 12, K = 13
+		std::cout << _WhoCard;
+		std::string JQK = "";
+		switch (_CurCard)
 		{
-			
-			std::cout << CheckCard[i][j];
+		case 11:
+			JQK = "J";
+			break;
+		case 12:
+			JQK = "Q";
+			break;
+		case 13:
+			JQK = "K";
+			break;
+		default:
+			break;
 		}
-		std::cout << std::endl;
-	}*/
-
-	//Cards Card;
-	//Card.CardDeclareNum();
-
-	//std::cout << "카드 : ";
-	//std::cout << std::endl;
-	//for (int j = 0; j < 4; j++)
-	//{
-	//	for (int i = 0; i < 13; i++)
-	//	{
-	//		std::cout << Card.CardArr[j][i] << " ";
-	//	}
-	//	std::cout << std::endl;
-	//}
+		std::cout << _Card.CardObject << " . " << JQK << std::endl;
+	}
+}
